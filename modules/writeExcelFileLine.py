7cec6604038671d4f2e589ad35d465342f005bea
jessy <jessy.martin38@gmail.com>
2021-11-09 10:27:24 +0100

    - writeExcelFileLine.py: cleaning comments, manage the change value, manage the fail use case.

--- modules/writeExcelFileLine.py
+++ modules/writeExcelFileLine.py
@@ -78,55 +78,32 @@ def run_module():
         line=dict(type='list', required=True)
     )
 
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # changed is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
     result = dict(
         changed=False,
         original_message='',
         message=''
     )
 
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True
     )
 
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
     if module.check_mode:
         module.exit_json(**result)
 
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-    # result['original_message'] = module.params['name']
-    # result['message'] = 'goodbye'
-
-    # use whatever logic you need to determine whether or not this module
-    # made any modifications to your target
-    # if module.params['new']:
-    #    result['changed'] = True
-
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
-    # if module.params['name'] == 'fail me':
-    #    module.fail_json(msg='You requested this to fail', **result)
-
     dest = module.params['dest']
-    exce_file = ExcelFile(dest)
-    exce_file.write_list_in_line_of_sheet(module.params['sheet'], module.params['line_number'],
-                                          module.params['line'], module.params['start_column'])
+    try:
+        excel_file = ExcelFile(dest)
+        excel_file.write_list_in_line_of_sheet(module.params['sheet'], module.params['line_number'],
+                                               module.params['line'], module.params['start_column'])
+        result['changed'] = True
 
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
+    except FileNotFoundError:
+        module.fail_json(msg="Impossible to create or find " + dest + ". Please check the file path.",
+                         **result)
+        pass
+
     module.exit_json(**result)
 
 
